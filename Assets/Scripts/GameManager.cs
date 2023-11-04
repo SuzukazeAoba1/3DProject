fe705bc4c54ca2200b5d71f16b55c23dbfa3e01b
rasillrus <rasillrus@naver.com>
2023-11-04 22:15:46 +0900

    test upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public float playTime;
+    public float playerLocation;
+    public float playerLaps;
+
+    public bool gameLose;
+    public bool gameWin;
+
+}
